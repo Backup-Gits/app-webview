088c71f1f27593e76d9aa367c99f73502ab37cdc
Diab Neiroukh <officiallazerl0rd@gmail.com>
2019-12-17 21:54:33 +0000

    🔥

---
+++ Android.mk
@@ -0,0 +1,22 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE := BromiteWebView
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MULTILIB := both
+LOCAL_OVERRIDES_PACKAGES := SystemWebView
+LOCAL_PRIVILEGED_MODULE := true
+
+ifeq ($(TARGET_ARCH), arm)
+LOCAL_SRC_FILES := prebuilt/arm_SystemWebView.apk
+endif
+ifeq ($(TARGET_ARCH), arm64)
+LOCAL_SRC_FILES := prebuilt/arm64_SystemWebView.apk
+endif
+ifeq ($(TARGET_ARCH), $(filter $(TARGET_ARCH), x86 x86_64))
+LOCAL_SRC_FILES := prebuilt/x86_SystemWebView.apk
+endif
+
+include $(BUILD_PREBUILT)
